bd8291121abdcc13bc41a6468a183958a5e265ef
Andrew Wood <aw.subs@outlook.com>
2018-07-30 11:33:58 +0000

    Adding more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,19 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 25' do
+    expect(fizzbuzz(25)).to eq 'buzz'
+  end
+  it 'returns "2" when passed 2' do
+    expect(fizzbuzz(2)).to eq '2'
+  end
 end
